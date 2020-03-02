82a135a047e436ba4a28c34f93f144ad0f56f876
weeenyoz <peterleewenyaoalpha@gmail.com>
2020-03-02 12:43:02 +0800

    add sign up and home component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,40 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
 
-import { SignupService } from './signup.service';
+import { SignupService } from "./signup.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit, OnDestroy {
-
-  authenticationStatusSubscription: Subscription
+  authenticationStatusSubscription: Subscription;
   signUpForm: FormGroup;
   isInconsistentPw: boolean;
   isLoading: boolean;
 
-  constructor(
-    private signupService: SignupService
-  ) { }
+  constructor(private signupService: SignupService) {}
 
   ngOnInit() {
-
     // start listening, whether user is signed up
-    this.authenticationStatusSubscription = this.signupService.getUserAuthenticatedStatus().subscribe(
-      (authenticationStatus:boolean) => {
+    this.authenticationStatusSubscription = this.signupService
+      .getUserAuthenticatedStatus()
+      .subscribe((authenticationStatus: boolean) => {
         this.isLoading = authenticationStatus;
-      }
-    )
+      });
 
     this.signUpForm = new FormGroup({
-      username: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),      
-      email: new FormControl('', {validators: [Validators.required, Validators.email, Validators.minLength(3)]}),
-      firstName: new FormControl('', {validators: [Validators.required]}),
-      lastName: new FormControl('', {validators: [Validators.required]}),
-      password: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]})
+      username: new FormControl("", {
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      email: new FormControl("", {
+        validators: [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(3)
+        ]
+      }),
+      firstName: new FormControl("", { validators: [Validators.required] }),
+      lastName: new FormControl("", { validators: [Validators.required] }),
+      password: new FormControl("", {
+        validators: [Validators.required, Validators.minLength(6)]
+      })
     });
   }
 
@@ -44,6 +50,7 @@ export class SignupComponent implements OnInit, OnDestroy {
 
   onSignUp() {
     if (this.signUpForm.invalid) {
+      console.log("invalid");
       return;
     } else {
       this.isLoading = true;
@@ -52,10 +59,9 @@ export class SignupComponent implements OnInit, OnDestroy {
         newUsername: username,
         newEmail: email,
         newPassword: password
-      }
+      };
       this.signupService.createUser(newUser);
       this.signUpForm.reset();
     }
   }
-
 }
--- src/app/signup/signup.service.ts
+++ src/app/signup/signup.service.ts
@@ -1,37 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SignupService {
-
   private userAuthenticatedSubjectListener = new Subject();
-  private backendUrl: String = environment.apiUrl + 'users';
+  private backendUrl: String = environment.apiUrl + "users";
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   getUserAuthenticatedStatus() {
     return this.userAuthenticatedSubjectListener.asObservable();
   }
 
   createUser(newUser) {
-    return this.http.post<{message: string, user: any}>(`${this.backendUrl}/signup`, newUser).subscribe(
-      (res: {message: string, user: any}) => {
-        this.router.navigate(['auth', 'login']);
-        return res;
-      }, 
-      error => {
-        console.log(error);
-        this.userAuthenticatedSubjectListener.next(false);
-      }
-    );
-  }
+    console.log("this.backendUrl:", this.backendUrl);
 
+    return this.http
+      .post<{ message: string; user: any }>(
+        `${this.backendUrl}/signup`,
+        newUser
+      )
+      .subscribe(
+        (res: { message: string; user: any }) => {
+          this.router.navigate(["auth", "login"]);
+          return res;
+        },
+        error => {
+          console.log("Error*", error);
+          this.userAuthenticatedSubjectListener.next(false);
+        }
+      );
+  }
 }
